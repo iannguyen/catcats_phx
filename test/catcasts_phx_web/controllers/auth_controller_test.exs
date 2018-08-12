e6ec66b7318548f7dbb53c08d12e72f375aa2096
Ian Nguyen <ian.nguyen@procore.com>
2018-08-11 19:33:51 -0700

    add spec for stubbing google callback

--- test/catcasts_phx_web/controllers/auth_controller_test.exs
+++ test/catcasts_phx_web/controllers/auth_controller_test.exs
@@ -1,9 +1,33 @@
 defmodule CatcastsPhxWeb.AuthControllerTest do
   use CatcastsPhxWeb.ConnCase
+  alias CatcastsPhx.Repo
+  alias CatcastsPhx.User
 
   test "redirects user to Google for authentication", %{conn: conn} do
     conn = get conn, "/auth/google"
     assert redirected_to(conn, 302)
   end
-end
 
+  @ueberauth %{
+    credentials: %{
+      token: 'asdfasdfasdfasdf',
+    },
+    info: %{
+      email: 'pusheen@pmd.com',
+      first_name: 'Pusheen',
+      last_name: 'The Cat',
+      provider: :google
+    }
+  }
+
+  test "creates the user from Google information", %{conn: conn} do
+    conn = conn
+           |> assign(:ueberauth, @ueberauth)
+           |> get("/auth/google/callback")
+
+    users = User |> Repo.all
+
+    assert Enum.count(users) == 1
+    assert get_flash(conn, :info) =~ "Thank you for signing in!"
+  end
+end
